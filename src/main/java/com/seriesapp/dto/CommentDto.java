2f78d00f532b40d8f42e08d50f28e258fe2acc22
VIKTOR <viktor.java.martynov@gmail.com>
2026-05-19 20:56:37 +0700

    add series

--- src/main/java/com/seriesapp/dto/CommentDto.java
+++ src/main/java/com/seriesapp/dto/CommentDto.java
@@ -7,7 +7,7 @@ import lombok.Data;
 public class CommentDto {
 
     @NotBlank(message = "Комментарий не может быть пустым")
-    @Size(min = 5, max = 2000, message = "Длина комментария: 5-2000 символов")
+    @Size(min = 5, max = 500, message = "Длина комментария: 5-500 символов")
     private String content;
 
     @Min(value = 1, message = "Минимальная оценка: 1")
